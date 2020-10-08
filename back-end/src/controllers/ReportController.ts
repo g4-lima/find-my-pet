b2772c5678bf293517d19990f21b0b6e5239bd64
Gabriel Lima <gabastos.lima@gmail.com>
2020-10-07 21:54:01 -0300

    put and delete

--- back-end/src/controllers/ReportController.ts
+++ back-end/src/controllers/ReportController.ts
@@ -3,7 +3,15 @@ import { getRepository } from 'typeorm';
 import Report from '../models/Report';
 
 export default class ReportController {
-  public async index(request: Request, response: Response): Promise<Response> {
+  public async get(request: Request, response: Response): Promise<any> {
+    const reportRepository = getRepository(Report);
+
+    const report = await reportRepository.find();
+
+    return response.json(report);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
     const {
       responsible,
       whatsapp,
@@ -27,5 +35,29 @@ export default class ReportController {
     await reportRepository.save(report);
 
     return response.json(report);
+  }
+
+  public async delete(request: Request, response: Response): Promise<any> {
+    const { id } = request.params;
+
+    const reportRepository = getRepository(Report);
+
+    await reportRepository.delete(id);
+
+    return response.status(200).json({ message: 'report deleted' });
+  }
+
+  public async put(request: Request, response: Response): Promise<any> {
+    const { id } = request.params;
+
+    const reportRepository = getRepository(Report);
+
+    const report = await reportRepository.findOne(id);
+
+    reportRepository.merge(report, request.body);
+
+    const updatedReport = await reportRepository.save(report);
+
+    return response.send(updatedReport);
   }
 }
--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -5,6 +5,9 @@ import ReportController from '../controllers/ReportController';
 const routes = Router();
 const reportController = new ReportController();
 
-routes.post('/report', reportController.index);
+routes.post('/report', reportController.create);
+routes.get('/list-reports', reportController.get);
+routes.delete('/report/:id', reportController.delete);
+routes.put('/report/:id', reportController.put);
 
 export default routes;
