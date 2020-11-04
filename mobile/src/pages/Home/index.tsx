6855d2726daf3628f017303f996decbe8a1a2c6c
Gabriel Lima <gabastos.lima@gmail.com>
2020-11-03 23:58:19 -0300

    home

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Image, Title } from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Image source="/" />
+      <Title />
+    </Container>
+  );
+};
+
+export default Home;
---
+++ mobile/src/pages/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex-direction: column;
+`;
+
+export const Image = styled.ImageBackground`
+  height: 80vh;
+`;
+
+export const Title = styled.Text``;
