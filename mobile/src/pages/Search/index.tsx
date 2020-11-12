058bdfee62211cb74221279e28885d90ec0ccbeb
Gabriel Lima <gabastos.lima@gmail.com>
2020-11-11 22:07:35 -0300

    card

--- mobile/src/pages/Search/index.tsx
+++ mobile/src/pages/Search/index.tsx
@@ -3,16 +3,36 @@ import {
   NavigationState,
   NavigatorScreenParams,
 } from '@react-navigation/native';
-import React from 'react';
+import React, { useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, TopBar, GoBackButton, Title } from './styles';
+import {
+  Container,
+  TopBar,
+  GoBackButton,
+  Title,
+  Card,
+  Image,
+  Infos,
+  CardHeader,
+  PetName,
+  Location,
+  CardBody,
+  DescriptionTitle,
+  Description,
+  Bottom,
+  Responsible,
+  WhatsappButton,
+  ShowButton,
+} from './styles';
 
 interface Props {
   navigation: NavigationProp<NavigationState, NavigatorScreenParams>;
 }
 
 const Search: React.FC<Props> = ({ navigation }) => {
+  const [show, setShow] = useState(false);
+
   return (
     <Container>
       <TopBar>
@@ -21,6 +41,41 @@ const Search: React.FC<Props> = ({ navigation }) => {
         </GoBackButton>
         <Title>Pesquisar Pets</Title>
       </TopBar>
+
+      <Card hide-show={show}>
+        <Image
+          source={{
+            uri:
+              'https://images.unsplash.com/photo-1566505512236-9723b3ab54fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+          }}
+        />
+        <Infos>
+          <CardHeader>
+            <PetName>Bob</PetName>
+            <Location>2 km</Location>
+          </CardHeader>
+
+          {show ? (
+            <CardBody>
+              <DescriptionTitle>Descrição:</DescriptionTitle>
+              <Description>
+                Cachorro da raça xxx, perdido no dia 02/02/2020.
+              </Description>
+              <Bottom>
+                <Responsible>Responsável: Gabriel</Responsible>
+                <WhatsappButton />
+              </Bottom>
+            </CardBody>
+          ) : null}
+          <ShowButton onPress={() => setShow(!show)}>
+            {show ? (
+              <Icon name="expand-less" size={56} color="#222a36" />
+            ) : (
+              <Icon name="expand-more" size={56} color="#222a36" />
+            )}
+          </ShowButton>
+        </Infos>
+      </Card>
     </Container>
   );
 };
--- mobile/src/pages/Search/styles.ts
+++ mobile/src/pages/Search/styles.ts
@@ -29,3 +29,85 @@ export const Title = styled.Text`
   margin: auto;
   padding-right: 80px;
 `;
+
+export const Card = styled.View`
+  background-color: #fff;
+  width: 95%;
+  height: 520px;
+  margin-top: 16px;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 4px;
+`;
+
+export const Image = styled.Image`
+  width: 100%;
+  height: 240px;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+`;
+
+export const Infos = styled.View``;
+
+export const CardHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const PetName = styled.Text`
+  font-family: Roboto-Bold;
+  font-size: 32px;
+  margin-left: 8px;
+  color: #222a36;
+`;
+
+export const Location = styled.Text`
+  color: #707070;
+  font-family: Roboto-Bold;
+  margin-right: 16px;
+  font-size: 12px;
+`;
+
+export const CardBody = styled.View`
+  margin: 8px;
+`;
+
+export const DescriptionTitle = styled.Text`
+  font-family: Roboto-Light;
+  color: #707070;
+  font-size: 12px;
+  margin-top: 12px;
+  margin-bottom: 4px;
+`;
+
+export const Description = styled.Text`
+  font-family: Roboto-Regular;
+  color: #222a36;
+  font-size: 16px;
+  height: 80px;
+`;
+
+export const Bottom = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Responsible = styled.Text`
+  font-family: Roboto-Light;
+  color: #707070;
+  font-size: 13px;
+`;
+
+export const WhatsappButton = styled.TouchableOpacity`
+  width: 160px;
+  height: 48px;
+  border-radius: 4px;
+  background-color: green;
+`;
+
+export const ShowButton = styled.TouchableOpacity`
+  margin: auto;
+  padding-top: 8px;
+`;
